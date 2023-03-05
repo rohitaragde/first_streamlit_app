92d8aed93ccc4591d2b5b9efc903f73a038a4995
rohitaragde <32512875+rohitaragde@users.noreply.github.com>
2023-03-05 17:30:09 -0500

    Update streamlit_app.py
    Updated the changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Moms New Healthy Diner')
 
@@ -8,4 +9,7 @@ streamlit.text('🥤thie Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
                 
 streamlit.header('🍇🍉Build Your own Fruit Smoothie🍎🍍')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
