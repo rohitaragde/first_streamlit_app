4481a7d89245167a530f31ccb158ebada3971cd3
rohitaragde <32512875+rohitaragde@users.noreply.github.com>
2023-03-09 17:11:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,14 +31,14 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+ fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information");
   else:
-streamlit.write('The user entered ', fruit_choice)
+     streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
 # write your own comment -what does the next line do? 
