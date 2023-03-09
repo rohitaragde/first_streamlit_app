6e33086202e19877ba169200b4012aed0eebe2bf
rohitaragde <32512875+rohitaragde@users.noreply.github.com>
2023-03-09 17:13:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,12 +39,10 @@ try:
 
 
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+      # write your own comment -what does the next line do? 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # write your own comment - what does this do?
+      streamlit.dataframe(fruityvice_normalized)
 
 #Dont run anything past here we troubleshoot
 except URLError as e:
