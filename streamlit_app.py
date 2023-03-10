7a0063133632d8cd86efc9d4fc317d0095ae1b70
rohitaragde <32512875+rohitaragde@users.noreply.github.com>
2023-03-10 17:39:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,9 +73,5 @@ def insert_row_snowflake(new_fruit):
      my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
      back_from_function=insert_row_snowflake(add_my_fruit)
      streamlit.text(back_from_function)
-                                    
-
-
-       
 
 
